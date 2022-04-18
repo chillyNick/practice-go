731386493ef107886b8050055806eb1e4fe6dc40
Nickolay Mizenko <mizenko1998@gmail.com>
2022-04-18 14:54:14 +0300

    coins solutions

--- coins/coins.go
+++ coins/coins.go
@@ -1,6 +1,19 @@
 package coins
 
+import "math"
 
 func Piles(n int) int {
-	return 0
+	return calc(n, n)
+}
+
+func calc(maxSize, avlCoins int) int {
+	if avlCoins == 0 {
+		return 1
+	}
+	r := 0
+	for c := int(math.Min(float64(avlCoins), float64(maxSize))); c > 0; c-- {
+		r += calc(c, avlCoins-c)
+	}
+
+	return r
 }
