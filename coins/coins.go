1c94bab2f0ffb6caa2a0b77db1cdf5765842eb26
Nickolay Mizenko <mizenko1998@gmail.com>
2022-04-18 15:17:25 +0300

    coins solution improvment

--- coins/coins.go
+++ coins/coins.go
@@ -1,8 +1,16 @@
 package coins
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
+
+var buff map[string]int
 
 func Piles(n int) int {
+	if len(buff) == 0 {
+		buff = make(map[string]int)
+	}
 	return calc(n, n)
 }
 
@@ -11,9 +19,17 @@ func calc(maxSize, avlCoins int) int {
 		return 1
 	}
 	r := 0
-	for c := int(math.Min(float64(avlCoins), float64(maxSize))); c > 0; c-- {
+	maxSize = int(math.Min(float64(avlCoins), float64(maxSize)))
+	k := strconv.Itoa(maxSize) + ":" + strconv.Itoa(avlCoins)
+	if rb, ok := buff[k]; ok {
+		return rb
+	}
+
+	for c := maxSize; c > 0; c-- {
 		r += calc(c, avlCoins-c)
 	}
+
+	buff[k] = r
 
 	return r
 }
