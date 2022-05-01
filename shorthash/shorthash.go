768c696d9f6502988315471ac5411781abcf0a8a
Nickolay Mizenko <mizenko1998@gmail.com>
2022-05-01 15:27:24 +0300

    shorthash solution

--- shorthash/shorthash.go
+++ shorthash/shorthash.go
@@ -1,5 +1,25 @@
 package shorthash
 
-func GenerateShortHashes(dictionary string, len int) []string {
-	return []string{}
+var hashes []string
+
+func GenerateShortHashes(dictionary string, maxLen int) []string {
+	dict := []rune(dictionary)
+	hashes = make([]string, 0)
+	for l := maxLen; l > 0; l-- {
+		addToHashes(dict, make([]rune, l), 0)
+	}
+
+	return hashes
+}
+
+func addToHashes(dict []rune, buf []rune, i int) {
+	if len(buf) == i {
+		hashes = append(hashes, string(buf))
+		return
+	}
+
+	for _, r := range dict {
+		buf[i] = r
+		addToHashes(dict, buf, i+1)
+	}
 }
